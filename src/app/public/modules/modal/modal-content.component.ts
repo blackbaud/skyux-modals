e56a536c94f553ce805f9adca35f6a9112f481b9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-02 13:21:40 -0400

    Initial port

---
+++ src/app/public/modules/modal/modal-content.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-modal-content',
+  templateUrl: '../shared/simple-content.html'
+})
+export class SkyModalContentComponent { }
---
+++ src/app/public/modules/modal/modal-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-modal-header',
+  templateUrl: '../shared/simple-content.html'
+})
+export class SkyModalHeaderComponent {
+}
