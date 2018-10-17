b2de4965e46763602cf1cc1e4f6fafa6d3ef2339
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-17 11:14:16 -0400

    Visual Tests > Also show overlay

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -3,33 +3,40 @@ import {
 } from '@angular/core';
 
 import {
+  SkyTilesModule
+} from '@skyux/tiles';
+
+import {
   SkyConfirmModule,
   SkyModalModule,
   SkyModalService
 } from './public';
 
 import {
-  SkyTilesModule
-} from '@skyux/tiles';
-
-import {
   ModalDemoComponent
 } from './visual/modal/modal-demo.component';
+
 import {
   ModalContentDemoComponent
 } from './visual/modal/modal-content-demo.component';
+
 import {
   ModalFullPageDemoComponent
 } from './visual/modal/modal-fullpage-demo.component';
+
 import {
   ModalLargeDemoComponent
 } from './visual/modal/modal-large-demo.component';
+
 import {
   ModalTiledDemoComponent
 } from './visual/modal/modal-tiled-demo.component';
+
 import {
   ModalContentAutofocusComponent
 } from './visual/modal/modal-content-autofocus.component';
+
+require('style-loader!./visual.scss');
 
 @NgModule({
   imports: [
