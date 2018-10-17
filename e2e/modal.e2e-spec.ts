449d45be08f5dd20429558997c8f117742e03258
Conor Wright <conor.wright@blackbaud.com>
2018-10-17 11:45:57 -0400

    Fixed modal to recognize autofocus. (#3)

--- e2e/modal.e2e-spec.ts
+++ e2e/modal.e2e-spec.ts
@@ -13,41 +13,65 @@ describe('Modal', () => {
     SkyHostBrowser.get('visual/modal');
   });
 
-  afterEach((done) => {
-    expect('.sky-modal').toMatchBaselineScreenshot(done);
-  });
-
   describe('(lg screens)', () => {
     beforeEach(() => {
       SkyHostBrowser.setWindowBreakpoint('lg');
     });
 
-    it('should match previous modal screenshot', () => {
+    it('should match previous modal screenshot', (done) => {
       element(by.css('.sky-btn-primary')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-screenshot'
+      });
     });
 
-    it('should match previous modal screenshot with help button in header', () => {
+    it('should match previous modal screenshot with help button in header', (done) => {
       element(by.css('.sky-modal-with-help')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-with-help-screenshot'
+      });
     });
 
-    it('should match previous screenshot of modal without header or footer', () => {
+    it('should match previous screenshot of modal without header or footer', (done) => {
       element(by.css('.sky-test-content-only')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-content-screenshot'
+      });
     });
 
-    it('should match previous small size modal screenshot', () => {
+    it('should match previous small size modal screenshot', (done) => {
       element(by.css('.sky-test-small-size-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-small-size-screenshot'
+      });
     });
 
-    it('should match previous medium size modal screenshot', () => {
+    it('should match previous medium size modal screenshot', (done) => {
       element(by.css('.sky-test-medium-size-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-medium-size-screenshot'
+      });
     });
 
-    it('should match previous large modal screenshot', () => {
+    it('should match previous large modal screenshot', (done) => {
       element(by.css('.sky-test-large-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-large-screenshot'
+      });
     });
 
-    it('should match previous large size modal screenshot', () => {
+    it('should match previous large size modal screenshot', (done) => {
       element(by.css('.sky-test-large-size-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-large-size-screenshot'
+      });
+    });
+
+    it('should match previous autofocus screenshot', (done) => {
+      element(by.css('.sky-test-large-modal-autofocus')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-lg-autofocus-screenshot'
+      });
     });
   });
 
@@ -56,12 +80,18 @@ describe('Modal', () => {
       SkyHostBrowser.setWindowBreakpoint('md');
     });
 
-    it('should match previous large size modal screenshot on intermediate screens', () => {
+    it('should match previous large size modal screenshot on intermediate screens', (done) => {
       element(by.css('.sky-test-large-size-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-md-large-size-screenshot'
+      });
     });
 
-    it('should match previous tiled modal screenshot', () => {
+    it('should match previous tiled modal screenshot', (done) => {
       element(by.css('.sky-test-tiled-modal')).click();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: 'modal-md-tiled-screenshot'
+      });
     });
   });
 
