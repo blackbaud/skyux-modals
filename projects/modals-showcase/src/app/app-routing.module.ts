46d30765126842ddd92b4ab085342ec788c084b7
Paul Crowder <paul.crowder@blackbaud.com>
2021-08-24 14:07:08 -0400

    Adopt Angular CLi

---
+++ projects/modals-showcase/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { VisualComponent } from './visual/visual.component';
+
+import { ConfirmVisualComponent } from './visual/confirm/confirm-visual.component';
+
+import { ModalVisualComponent } from './visual/modal/modal-visual.component';
+
+const routes: Routes = [
+  {
+    path: 'visual',
+    component: VisualComponent
+  },
+  {
+    path: 'visual/confirm',
+    component: ConfirmVisualComponent
+  },
+  {
+    path: 'visual/modal',
+    component: ModalVisualComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
